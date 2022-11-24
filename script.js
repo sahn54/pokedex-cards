6db1a49dbe524ecbf01dd42b0834959a5302bd89
sahn54 <s.ahn7@icloud.com>
2022-11-23 21:40:27 -0500

    Api-commit-S

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const poke_container = document.getElementById('poke-container')
 const pokemon_count = 150
-const color = {
+const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
     electric: 'FCF7DE',
@@ -17,7 +17,12 @@ const color = {
     normal: '#F5F5F5',
 }
 
-const fetchPokemons = () => {
+const main_types = Object.keys(colors)
+
+
+
+
+const fetchPokemons = async() => {
     for(let i = 1 ; i <= pokemon_count; i++){
         await getPokemon(i)
     }
@@ -27,7 +32,54 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+    createPokemonCard(data)
 }
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    let idSecondDigit = () => {
+        if (pokemon.id <= 9) {
+           return 0
+        } else if (pokemon.id >= 100) {
+           return ''
+        } else {
+           return ''
+        }
+     }
+   
+     let idThirdDigit = () => {
+        if (pokemon.id >= 100) {
+           return ''
+        } else {
+           return 0
+        }
+     }
+
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3,'0')
+    const poke_types = pokemon.types.map(type =>type.type.name)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const color = colors[type]
+
+    pokemonEl.style.backgroundColor= color
+
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${idThirdDigit()}${idSecondDigit()}${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span></small>
+    </div>    
+    `
+
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    poke_container.appendChild(pokemonEl)
+}
+
 
 fetchPokemons();
